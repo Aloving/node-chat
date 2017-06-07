ec6a8b01a5daae2e6172b09ed201be55049bce8e
Aloving <Ilegend2011@gmail.com>
2017-06-07 13:06:30 +0300

    socket io module

---
+++ socket/index.js
@@ -0,0 +1,14 @@
+module.exports = function(server){	
+	var io = require('socket.io').listen(server);
+
+	io.set('origins', 'localhost:*');
+
+	io.sockets.on('connection', function (socket) {
+
+	  socket.on('message', function (text, cb) {
+	    socket.broadcast.emit('message', text);
+	    cb("123");
+	  });
+
+	});
+}
